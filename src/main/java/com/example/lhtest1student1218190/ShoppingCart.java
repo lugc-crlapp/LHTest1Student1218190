638040c679164623f319ef56ed69ceb29e15d245
Cameron Robert Lapp <crlapp@lakeheadu.ca>
2024-02-05 13:58:35 -0500

    Question 3.4 complete

--- src/main/java/com/example/lhtest1student1218190/ShoppingCart.java
+++ src/main/java/com/example/lhtest1student1218190/ShoppingCart.java
@@ -1,5 +1,7 @@
 package com.example.lhtest1student1218190;
 
+import jdk.internal.access.JavaLangInvokeAccess;
+
 import java.util.ArrayList;
 
 public class ShoppingCart {
@@ -20,6 +22,14 @@ public class ShoppingCart {
             totalPrice += item.getPrice();
         }
         return totalPrice;
+    }
+
+    public String getComponentNames() {
+        String names = "";
+        for (Component item : items) {
+            names += item.getName() + ", ";
+        }
+        return names;
     }
 
 
