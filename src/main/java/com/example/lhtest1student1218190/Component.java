8fb700baba7782731256fce53b40d632722c991f
Cameron Robert Lapp <crlapp@lakeheadu.ca>
2024-02-05 13:09:49 -0500

    Question 2.1 complete

---
+++ src/main/java/com/example/lhtest1student1218190/Component.java
@@ -0,0 +1,16 @@
+package com.example.lhtest1student1218190;
+
+public class Component {
+private String name;
+    private String manufacturer;
+    private double price;
+
+    public Component(String name, String manufacturer, double price) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.price = price;
+    }
+
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lhtest1student1218190 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.lhtest1student1218190 to javafx.fxml;
+    exports com.example.lhtest1student1218190;
+}
